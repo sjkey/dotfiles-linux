12da62e47653ca31bda2b2674b3cb6fea048a130
sjkey <kuba.serwin99@gmail.com>
2024-06-27 19:44:27 +0200

    v1: update paths

--- init.sh
+++ init.sh
@@ -33,20 +33,20 @@ curl -fsSL https://get.pnpm.io/install.sh | sh -
 # Starship
 curl -sS https://starship.rs/install.sh | sh
 mkdir -p ~/.config
-cp config/starship.toml ~/.config/starship.toml
+cp ./config/starship.toml ~/.config/starship.toml
 # TODO: add required stuff to bashrc
 
 # VS Code
-chmod +x scripts/vscode.sh
+chmod +x ./scripts/vscode.sh
 ./scripts/vscode.sh
 
 # Nerd Fonts
-mkdir -p ~/.local/share/fonts && cd ~/.local/share/fonts
-curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DejaVuSansMono/Regular/DejaVuSansMNerdFont-Regular.ttf
+mkdir -p ~/.local/share/fonts
+curl -fLo ~/.local/share/fonts/DejaVuSansMNerdFont-Regular.ttf https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DejaVuSansMono/Regular/DejaVuSansMNerdFont-Regular.ttf
 fc-cache -fv
 
 # Terminal
-dconf load /org/gnome/terminal/ < config/terminal.dconf
+dconf load /org/gnome/terminal/ < ./config/terminal.dconf
 
 # i3
 sudo apt install -y feh i3
@@ -59,9 +59,8 @@ sudo apt install -y feh i3
 
 # Polybar
 sudo apt install -y polybar xdotool
-# TODO: FIX needed
-sudo cp polybar/config.ini /etc/polybar/config.ini
-chmod +x polybar/launch.sh
+sudo cp ./polybar/config.ini /etc/polybar/config.ini
+chmod +x ./polybar/launch.sh
 # ./polybar/launch.sh
 
 sudo reboot

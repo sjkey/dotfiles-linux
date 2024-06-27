525c8847e5e92b5515f62963adbd81013cb1ed32
sjkey <kuba.serwin99@gmail.com>
2024-06-27 08:45:18 +0200

    v1: polybar

--- init.sh
+++ init.sh
@@ -3,7 +3,21 @@ set -e
 
 sudo apt update && sudo apt upgrade -y
 
-# Install NVM and Node.js
+# Remove unwanted pre-installed applications
+sudo snap remove firefox thunderbird
+sudo apt purge -y yelp libreoffice* rhythmbox firefox* thunderbird* shotwell gnome-clocks gnome-calculator remmina gnome-calendar gnome-screenshot gnome-text-editor transmission-common transmission-gtk
+sudo snap remove snap-store
+sudo apt autoremove -y
+
+# Install essential tools
+sudo apt install -y curl git dconf-cli
+
+# Tools to consider installing later:
+# tmux
+# gnome-tweaks
+# docker.io
+
+# NVM and Node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -12,8 +26,9 @@ nvm install node
 node -v
 npm -v
 
-# Install pnpm
+# pnpm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
+source ~/.bashrc
 pnpm -v
 
 # Starship
@@ -21,3 +36,24 @@ curl -sS https://starship.rs/install.sh | sh
 mkdir -p ~/.config
 cp config/starship.toml ~/.config/starship.toml
 
+# Terminal
+dconf load /org/gnome/terminal/ < config/terminal.dconf
+
+# VS Code
+chmod +x scripts/vscode.sh
+./scripts/vscode.sh
+
+# i3
+sudo apt install -y feh i3
+# cp config/wallpaper.jpg ~/Pictures/wallpaper.jpg
+
+# copy i3 config when it's ready
+# for now let's use 
+# echo 'exec --no-startup-id feh --bg-scale ~/Pictures/wallpaper.jpg' >> ~/.config/i3/config
+# i3-msg restart
+
+# Polybar
+sudo apt install -y polybar xdotool
+cp polybar/config.ini /etc/polybar/config.ini
+chmod +x polybar/launch.sh
+./polybar/launch.sh
---
+++ polybar/launch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xdotool search --all --screen $DISPLAY --classname polybar windowkill %@
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown &

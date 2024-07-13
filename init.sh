00e32b224a0a4c035465aa2a8cd06282516a34ba
sjkey <kuba.serwin99@gmail.com>
2024-07-13 11:59:12 +0200

    v1: update apt before brave install

--- init.sh
+++ init.sh
@@ -41,6 +41,7 @@ cp .vscode/keybindings.json ~/.config/Code/User/keybindings.json
 # Brave Browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
 sudo apt install -y brave-browser
 
 # Nerd Fonts

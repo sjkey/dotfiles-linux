30f8d4b6f9c9746e48ae6a42cfef975018802a6e
sjkey <kuba.serwin99@gmail.com>
2024-07-15 15:51:39 +0200

    v1: code

--- init.sh
+++ init.sh
@@ -40,13 +40,13 @@ cp ./config/starship.toml ~/.config/starship.toml
 cat ./config/.bashrc >> ~/.bashrc
 
 # Code
-# sudo apt-get install -y gpg
-# wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-# sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-# echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-# rm -f packages.microsoft.gpg
-# sudo apt update
-# sudo apt install -y code
+sudo apt-get install -y gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt update
+sudo apt install -y code
 mkdir -p ~/.config/Code/User
 cp .vscode/settings.json ~/.config/Code/User/settings.json
 cp .vscode/keybindings.json ~/.config/Code/User/keybindings.json
--- readme.md
+++ readme.md
@@ -29,17 +29,14 @@
 
 #### Installation
 
-1. Install Code
-2. Clone the repository and navigate to the directory
-3. Run the initialization script
+1. Clone the repository and navigate to the directory
+2. Run the initialization script
     ```bash
     chmod +x init.sh
     ./init.sh
     ```
-4. Select `i3` after reboot
-5. Run `follow-up.sh` and restart i3
-6. Install suggested Code extensions
-
+3. Select `i3` after reboot
+4. Run `follow-up.sh` and restart i3
 
 ### TODO
 - Mic status / mute toggle in Polybar

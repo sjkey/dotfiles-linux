407b2994ca83964e8eb6517d9e4e3f708bf45cc1
sjkey <kuba.serwin99@gmail.com>
2024-07-15 06:15:24 +0200

    v1: docker

--- init.sh
+++ init.sh
@@ -26,16 +26,11 @@ npm -v
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 
 # Docker
-sudo apt-get install -y ca-certificates
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo apt install -y apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce
 wget https://desktop.docker.com/linux/main/amd64/157355/docker-desktop-amd64.deb
 sudo apt install -y ./docker-desktop-amd64.deb
 
